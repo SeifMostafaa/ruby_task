91651ec24f3c3f926d7c08f2e5649b1a76c12565
Seif <seifMostafaa@bitbucket.com>
2022-09-20 00:02:49 +0200

    tests in main

--- main.rb
+++ main.rb
@@ -1,41 +1,45 @@
 require_relative "stack"
 require_relative "extras"
 
-# s = Stack.new
+puts "Start of Testing Stack"
 
-# s.push(10)
-# s.push(9)
-# s.push(8)
-# s.push(7)
-# s.push(6)
-# s.push(5)
-# s.push(4)
-# s.push(3)
-# s.push(2)
-# s.push(1)
-# s.push(100)
-# s.push(1000)
+s = Stack.new
 
-# puts s.max()
-# s.pop()
-# puts s.max()
-# s.pop()
-# puts s.max()
-# s.pop()
-# s.pop()
-# s.pop()
-# s.pop()
-# s.pop()
-# s.pop()
-# s.pop()
-# s.pop()
-# s.pop()
-# s.pop()
-# puts s.max()
+s.push(10)
+s.push(9)
+s.push(8)
+s.push(7)
+s.push(6)
+s.push(5)
+s.push(4)
+s.push(3)
+s.push(2)
+s.push(1)
+s.push(100)
+s.push(1000)
 
+puts s.max()
+s.pop()
+puts s.max()
+s.pop()
+puts s.max()
+s.pop()
+s.pop()
+s.pop()
+s.pop()
+s.pop()
+s.pop()
+s.pop()
+s.pop()
+s.pop()
+s.pop()
+puts s.max()
 
+puts "End of Testing Stack"
 
 ##################################################################
+
+puts "Start of Testing Extras"
 
 e = Extras.new
 
@@ -52,8 +56,6 @@ puts e.mean() #23.8
 e.push(100)
 puts e.mean() #36.5
 
-
-
 puts "=================="
 
 e.pop()
@@ -69,5 +71,4 @@ puts e.mean()
 e.pop()
 puts e.mean()
 
-
-
+puts "End of Testing Extras"
